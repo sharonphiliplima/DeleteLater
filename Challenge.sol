d13109829e5b61228f2ee42719d5173b46e212fc
Sharon Philip Lima <69852010+sharonphiliplima@users.noreply.github.com>
2023-08-02 02:50:24 +0530

    Update Challenge.sol

--- Challenge.sol
+++ Challenge.sol
@@ -3,16 +3,12 @@ pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "Challenge/9-Lesson.sol";
 
-// The ChallengeInterface must be implemented in a separate contract and define the challenge logic.
-abstract contract ChallengeInterface {
-    function solveChallenge(uint256 randomGuess, string memory yourTwitterHandle) external virtual;
-}
 
 contract Solution is IERC721Receiver {
     // Address of the ERC721 contract that holds the NFTs.
-    address public contractAddress = 0x33e1fD270599188BB1489a169dF1f0be08b83509;
-
+    LessonNine lessonNine;
     // Address of the receiver where the NFTs will be transferred.
     address public receiver = 0x54C20Af5C53008574Bd26621497f03086060E01F;
 
@@ -21,16 +17,14 @@ contract Solution is IERC721Receiver {
     // The constructor sets the contractAddress and correctAnswer based on some initialization logic.
     constructor() {
         correctAnswer = calculateCorrectAnswer();
+        lessonNine = LessonNine(0x33e1fD270599188BB1489a169dF1f0be08b83509);
     }
 
 
-    // Function to call the solveChallenge function on the ChallengeInterface contract.
-    function callSolveChallenge() external {
-        require(correctAnswer != 0, "Correct answer not set");
+    // Function to call the solveChallenge function on the Challenge (lesson 9)
+    function callSolveChallenge() external {        
         
-        ChallengeInterface contractInstance = 
-        ChallengeInterface(0x33e1fD270599188BB1489a169dF1f0be08b83509);
-        contractInstance.solveChallenge(correctAnswer, "sharonplima");
+        lessonNine.solveChallenge(correctAnswer, "sharonplima");
     }
 
     function calculateCorrectAnswer() internal view returns (uint256) {
