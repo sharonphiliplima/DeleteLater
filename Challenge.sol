cf9d89b0a2300670b27ba57490f647ec4fc2d851
Sharon Philip Lima <69852010+sharonphiliplima@users.noreply.github.com>
2023-08-02 20:08:34 +0530

    Update Challenge.sol

--- Challenge.sol
+++ Challenge.sol
@@ -1,52 +1,43 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
-
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+//SPDX-License-Identifier: MIT
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
-import "Challenge/9-Lesson.sol";
+import {LessonNine} from "./Challenge/9-Lesson.sol";
 
+pragma solidity ^0.8.18;
 
 contract Solution is IERC721Receiver {
     // Address of the ERC721 contract that holds the NFTs.
-    LessonNine lessonNine;
-    // Address of the receiver where the NFTs will be transferred.
-    address public receiver = 0x54C20Af5C53008574Bd26621497f03086060E01F;
+    address public contractAddress = 0x33e1fD270599188BB1489a169dF1f0be08b83509;
+    address owner;
 
     uint256 public correctAnswer;
+    LessonNine public lessonNine;
 
     // The constructor sets the contractAddress and correctAnswer based on some initialization logic.
     constructor() {
-        lessonNine = LessonNine(0x33e1fD270599188BB1489a169dF1f0be08b83509);
+        lessonNine = LessonNine(contractAddress);
+        owner = msg.sender;
     }
 
-
-    // Function to call the solveChallenge function on the Challenge (lesson 9)
-    function callSolveChallenge() external { 
-        
-        correctAnswer = calculateCorrectAnswer();
-       
-        
+    // Function to call the solveChallenge function on the ChallengeInterface contract.
+    function callSolveChallenge() external {
+        correctAnswer = uint256(keccak256(abi.encodePacked(address(this), block.prevrandao, block.timestamp))) % 100000;
         lessonNine.solveChallenge(correctAnswer, "sharonplima");
     }
 
-    function calculateCorrectAnswer() internal view returns (uint256) {
-        // logic to calculate the correct answer.
-        uint256 randomNumber = uint256(keccak256(abi.encodePacked(address(this), block.timestamp, block.prevrandao)));
-        return randomNumber % 100000;
-    }
-
-    // Function to receive ERC721 tokens. This function is called when NFTs are transferred to this contract.
-    function onERC721Received(
-        address operator,
-        address from,
-        uint256 tokenId,
-        bytes calldata data
-    ) external override returns (bytes4) {
-        // Perform the NFT transfer to the specified receiver address.
-        //https://sepolia.etherscan.io/address/0x76b50696b8effca6ee6da7f6471110f334536321#code
-        IERC721(0x76B50696B8EFFCA6Ee6Da7F6471110F334536321).safeTransferFrom(address(this), receiver, tokenId);
-
-        // Return the ERC721_RECEIVED value as per the ERC721 standard.
+    function onERC721Received(address operator, address from, uint256 tokenId, bytes calldata data)
+        public override returns (bytes4) {
         return this.onERC721Received.selector;
     }
+
+    function transferTime(address NFTcontract, uint256 tokenId)public {
+        ERC721 target = ERC721(NFTcontract);
+        target.transferFrom(address(this), owner, tokenId);
+    }
+
 }
+
+
+
+
+
